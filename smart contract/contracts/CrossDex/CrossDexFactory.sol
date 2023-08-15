40b892d256cb5bb213245dce9ca36a522b58000b
cs0041 <pamzalove20@hotmail.com>
2023-08-16 00:53:34 +0700

    CrossDexFactory

---
+++ smart contract/contracts/CrossDex/CrossDexFactory.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "./CrossDexPair.sol";
+import "./interface/interfaceCrossDexFactory.sol";
+ 
+
+contract CrossDexFactory is ICrossDexFactory {
+    mapping(address => mapping(address => address)) public getPair;
+    address[] public allPairs;
+
+
+    function allPairsLength() external view returns (uint) {
+        return allPairs.length;
+    }
+
+    function createPair(address token0, address token1) external returns (address pair) {
+        require(token0 != token1, "CrossDex: IDENTICAL_ADDRESSES");
+        require(token0 != address(0), "CrossDex: ZERO_ADDRESS");
+        require(getPair[token0][token1] == address(0), "CrossDex: PAIR_EXISTS"); // single check is sufficient
+        
+
+        bytes32 _salt = keccak256(abi.encodePacked(token0, token1));
+        CrossDexPair newPair = new CrossDexPair{salt: _salt}(token0,token1);
+        require(address(newPair) != address(0),"CrossDex: Fail_CreatePair");
+
+        getPair[token0][token1] = pair;
+        getPair[token1][token0] = pair; // populate mapping in the reverse direction
+        allPairs.push(pair);
+        emit PairCreated(token0, token1, pair, allPairs.length);
+    }
+
+}
