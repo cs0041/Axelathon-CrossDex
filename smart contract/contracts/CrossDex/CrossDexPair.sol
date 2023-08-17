f6f3904181283a63da60179218ef9093b28e4f31
cs0041 <pamzalove20@hotmail.com>
2023-08-18 00:25:20 +0700

    add logic useful for bridge
    -mint() , burn() for bridge lp
    -onlyRouterMainChain check for caller must from mainchain only
    -onlyAxelra  check for caller must from  axelrabridge only

--- smart contract/contracts/CrossDex/CrossDexPair.sol
+++ smart contract/contracts/CrossDex/CrossDexPair.sol
@@ -8,13 +8,31 @@ contract CrossDexPair is CrossDexERC20 {
     IERC20 public immutable token0;
     IERC20 public immutable token1;
 
+    address public immutable routerMainChain;
     uint256 public reserve0;
     uint256 public reserve1;
 
+    modifier onlyRouterMainChain() {
+        require(routerMainChain == msg.sender, "Ownable: caller is not RouterMainChain");
+        _;
+    }
 
-    constructor(address _token0, address _token1)  {
+    modifier onlyAxelra() {
+        require(msg.sender == axelraBridgeAddress ,"Ownable: caller is not axelraBridge");
+        _;
+    }
+
+    constructor(address _token0, address _token1,address _routerMainChain)  {
         token0 = IERC20(_token0);
         token1 = IERC20(_token1);
+        routerMainChain = _routerMainChain;
+    }
+
+    function mint(address to, uint256 amount) public onlyAxelra {
+        _mint(to, amount);
+    }
+    function burn(address to, uint256 amount) public onlyAxelra {
+         _burn(to, amount); 
     }
 
     
@@ -53,7 +71,7 @@ contract CrossDexPair is CrossDexERC20 {
         reserve1 = _reserve1;
     }
 
-    function swap(uint256 _amountIn,uint256 _amountOutMin, address _tokenIn,address to) external  returns (uint256 amountOut) {
+    function swap(uint256 _amountIn,uint256 _amountOutMin, address _tokenIn,address to) external onlyRouterMainChain returns (uint256 amountOut) {
         require( _tokenIn != address(0),"CrossDex: Address Zero");
         require( _tokenIn == address(token0) || _tokenIn == address(token1), "CrossDex: Invalid token");
         require(_amountIn > 0, "CrossDex: Amount Zero");
@@ -78,14 +96,14 @@ contract CrossDexPair is CrossDexERC20 {
         _update(token0.balanceOf(address(this)), token1.balanceOf(address(this)));
     }
 
-
-    function addLiquidity(uint256 _amount0, uint256 _amount1,address to,bool isForceAdd) external returns (uint256 liquidity) {
+    function addLiquidity(uint256 _amount0, uint256 _amount1,address to,bool isForceAdd) external onlyRouterMainChain returns (uint256 liquidity) {
         
         token0.transferFrom(msg.sender, address(this), _amount0);
         token1.transferFrom(msg.sender, address(this), _amount1);
 
         uint256 famount0;
         uint256 famount1;
+
 
         if (reserve0 > 0 || reserve1 > 0) {
             if(isForceAdd && reserve0 * _amount1 != reserve1 * _amount0){
@@ -102,7 +120,6 @@ contract CrossDexPair is CrossDexERC20 {
                 famount0 = _amount0;
                 famount1 = _amount1;
             }
-
 
             // check equation
             {
@@ -143,7 +160,7 @@ contract CrossDexPair is CrossDexERC20 {
         _update(token0.balanceOf(address(this)), token1.balanceOf(address(this)));
     }
 
-    function removeLiquidity(uint256 liquidity, address to) external returns (uint256 amount0, uint256 amount1) {
+    function removeLiquidity(uint256 liquidity, address to) external onlyRouterMainChain returns (uint256 amount0, uint256 amount1) {
         transferFrom(msg.sender, address(this), liquidity);
 
         uint256 bal0 = token0.balanceOf(address(this));
@@ -178,4 +195,5 @@ contract CrossDexPair is CrossDexERC20 {
     }
 }
 
+ 
  
