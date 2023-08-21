1809d1906a8f1bb5787979203f58efb51738fdc9
cs0041 <pamzalove20@hotmail.com>
2023-08-22 03:23:32 +0700

    Update swap.tsx

--- web/pages/swap.tsx
+++ web/pages/swap.tsx
@@ -1,21 +1,32 @@
-import React  from 'react'
+import React, { useState }  from 'react'
 import {
   Cog6ToothIcon,
   ArrowDownCircleIcon,
   InformationCircleIcon,
 } from '@heroicons/react/24/solid'
 import SVGLoader from '../components/SVGLoader'
+import MyModal from '../components/MyModal'
+import { useAccount } from 'wagmi'
+import MyListBox from '../components/MyListBox'
 
 
 type Props = {}
 
 function swap({}: Props) {
+   const { address } = useAccount()
+  const [showModalEditAddress, setShowModalEditAddress] = useState(false)
    return (
      <div className="flex mt-14 justify-center items-center">
-       <div className="bg-[#0D111C] px-2 py-3 rounded-xl border-[1px] border-[#fafafa4d]  max-w-[450px] min-w-[400px]">
+       <div className="bg-[#0D111C] px-2 py-3 rounded-3xl border-[1px] border-[#fafafa4d]  max-w-[450px] min-w-[400px]">
          <div className="flex flex-row justify-between items-center mb-2 px-2">
-           <h1 className="font-bold text-sm">Swap</h1>
-           <Cog6ToothIcon className="h-6 w-6 hover:opacity-60 cursor-pointer transition-all" />
+           <h1 className="font-bold text-base">Swap</h1>
+           <div
+             className="flex flex-row justify-center items-center gap-1 text-xs
+            hover:opacity-60 cursor-pointer transition-all bg-blue-600 rounded-2xl py-1 px-3"
+           >
+             <span>0.1% slippage</span>
+             <Cog6ToothIcon className="h-6 w-6 " />
+           </div>
          </div>
 
          <div className="InputOrder gap-1 pt-2 pb-3 px-4">
@@ -31,10 +42,9 @@ function swap({}: Props) {
                  }
                }}
              />
-             <button className=" gap-1 flex flex-row px-3 items-center justify-center bg-[#293249]  rounded-xl font-bold text-white cursor-pointer">
-               <img src="logo.png" alt="logo" className="w-6 " />
-               <span>USD</span>
-             </button>
+             <MyListBox
+               listItem={[{ text: 'ETH' }, { text: 'USDT' }, { text: 'AXL' }]}
+             />
            </div>
            <div className="flex justify-end gap-2 text-sm text-gray-400">
              <span> Balance: 123123</span>
@@ -53,7 +63,7 @@ function swap({}: Props) {
 
          <div className="InputOrder rounded-b-none gap-1 pt-2 pb-3 px-4">
            <span className="text-gray-500">You receive</span>
-           <div className="flex flex-row gap-2">
+           <div className="flex flex-row gap-2 ">
              <input
                className="text-3xl  w-full   text-left  bg-transparent outline-none  text-white"
                placeholder="0"
@@ -64,10 +74,9 @@ function swap({}: Props) {
                  }
                }}
              />
-             <button className=" gap-1 flex flex-row px-3 items-center justify-center bg-[#293249]  rounded-xl font-bold text-white cursor-pointer">
-               <img src="logo.png" alt="logo" className="w-6 " />
-               <span>USD</span>
-             </button>
+             <MyListBox
+               listItem={[{ text: 'ETH' }, { text: 'USDT' }, { text: 'AXL' }]}
+             />
            </div>
            <div className="flex justify-end gap-2 text-sm text-gray-400">
              <span> Balance: 123123</span>
@@ -99,13 +108,47 @@ function swap({}: Props) {
              <p>Price Impact</p>
              <p>Liquidity Provider Fee</p>
            </div>
-           <div className='text-right'>
+           <div className="text-right">
              <p>0.00</p>
              <p>0%</p>
              <p>0% fee</p>
            </div>
          </div>
+         <div
+           className="bg-[#121A2A] flex flex-col mt-2 rounded-lg py-2 px-4 justify-center items-start 
+           text-gray-300  text-xs  "
+         >
+           <div className="flex flex-row w-full items-center justify-between ">
+             <div>Recipient Address</div>
+             <div className="flex gap-1">
+               <span> 0x313..4124</span>
+               <span
+                 className="text-blue-500 underline cursor-pointer hover:opacity-60 transition-all"
+                 onClick={() => setShowModalEditAddress(true)}
+               >
+                 Edit
+               </span>
+             </div>
+           </div>
+           <div className="flex flex-row w-full items-center justify-between">
+             <div>Destination Chain ReceiveToken</div>
+             <MyListBox
+               listItem={[
+                 { text: 'Avalanche' },
+                 { text: 'Fantom' },
+                 { text: 'Polygon' },
+               ]}
+             />
+           </div>
+         </div>
        </div>
+
+       {showModalEditAddress && (
+         <MyModal
+           userAddress={address}
+           onClose={() => setShowModalEditAddress(false)}
+         />
+       )}
      </div>
    )
 }
