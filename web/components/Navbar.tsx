8cbf0c85564846a6bd009885c8ecdd9d15b7e7ad
cs0041 <pamzalove20@hotmail.com>
2023-08-22 00:32:36 +0700

    Update Narbar

--- web/components/Navbar.tsx
+++ web/components/Navbar.tsx
@@ -1,16 +1,64 @@
 import { ConnectButton } from '@rainbow-me/rainbowkit'
+import Link from 'next/link'
+import { Dispatch, SetStateAction, useState } from 'react'
 
-type Props = {}
+interface ItemNavbar {
+  title: string
+  link: string
+  currentPage: string
+  setCurrentPage: Dispatch<SetStateAction<string>>
+}
 
-function Navbar({}: Props) {
+const objTitlelink = [
+  { title: 'Swap', link: 'swap' },
+  { title: 'Liquidity', link: 'liquidity' },
+  { title: 'Stake', link: 'stake' },
+  { title: 'Faucet', link: 'faucet' },
+]
+
+const NavbarItem = ({ title, link, currentPage, setCurrentPage }:ItemNavbar) => {
+  return (
+    <Link href={`/${link}`}>
+      <div
+        onClick={() => setCurrentPage(link)}
+        className={`  cursor-pointer  transition ease-in-out  py-2 px-4 rounded-lg hover:bg-gray-800 ${
+          link == currentPage ? 'text-white' : 'text-[#98a1c0]'
+        } `}
+      >
+        {title}
+      </div>
+    </Link>
+  )
+}
+
+
+function Navbar() {
+   const [currentPage, setCurrentPage] = useState<string>(location.pathname.slice(1))
   return (
     <div className="sticky inset-0 z-10">
-      <div className="bg-black p-2">
-        <ConnectButton
-          label="connect web3"
-          accountStatus={'full'}
-          chainStatus={'full'}
-        />
+      <div className="flex flex-row items-center justify-between px-5 py-3 transition-all duration-200">
+        <div className="font-semibold text-base  flex flex-row justify-center items-center">
+          <div className="flex gap-1 mr-6  text-xl justify-center items-center mx-2">
+            <img src="/logo.png" alt="logo" className="h-9 w-9" />
+            {/* <h1>CrossDex</h1> */}
+          </div>
+          <div className="flex flex-row  items-center gap-2  ">
+            {objTitlelink.map((item, index) => (
+              <NavbarItem
+                key={item.title + index}
+                title={item.title}
+                link={item.link}
+                currentPage={currentPage}
+                setCurrentPage={setCurrentPage}
+              />
+            ))}
+          </div>
+        </div>
+          <ConnectButton
+            label="connect web3"
+            accountStatus={'full'}
+            chainStatus={'full'}
+          />
       </div>
     </div>
   )
