6a22b8ce93b6cd8ceff6e9c38643e3df1399a2c0
cs0041 <pamzalove20@hotmail.com>
2023-08-23 02:48:02 +0700

    change node modules hardhat

--- smart contract/hardhat.config.ts
+++ smart contract/hardhat.config.ts
@@ -1,5 +1,8 @@
 import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import 'hardhat-deploy'
+import '@typechain/hardhat'
+import '@nomiclabs/hardhat-ethers'
+import 'hardhat-gas-reporter'
 const config: HardhatUserConfig = {
   solidity: '0.8.19',
 }
