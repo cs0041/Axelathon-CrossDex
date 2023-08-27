3899dbfbc5cbc541d7a8098dbc718af2420f77a1
cs0041 <pamzalove20@hotmail.com>
2023-08-28 05:30:51 +0700

    fix bug pathname

--- web/components/Navbar.tsx
+++ web/components/Navbar.tsx
@@ -1,29 +1,28 @@
 import { ConnectButton } from '@rainbow-me/rainbowkit'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 import { Dispatch, SetStateAction, useState } from 'react'
 
 interface ItemNavbar {
   title: string
   link: string
-  currentPage: string
-  setCurrentPage: Dispatch<SetStateAction<string>>
+  pathname:string
 }
 
 const objTitlelink = [
-  { title: 'Swap', link: 'swap' },
-  { title: 'Liquidity', link: 'liquidity' },
-  { title: 'Bridge token', link: 'bridgetoken' },
-  { title: 'Faucet', link: 'faucet' },
-  { title: 'Status Axelar Bridge', link: 'statusaxelar' },
+  { title: 'Swap', link: '/swap' },
+  { title: 'Liquidity', link: '/liquidity' },
+  { title: 'Bridge token', link: '/bridgetoken' },
+  { title: 'Faucet', link: '/faucet' },
+  { title: 'Status Axelar Bridge', link: '/statusaxelar' },
 ]
 
-const NavbarItem = ({ title, link, currentPage, setCurrentPage }:ItemNavbar) => {
+const NavbarItem = ({ title, link, pathname }: ItemNavbar) => {
   return (
-    <Link href={`/${link}`}>
+    <Link href={`${link}`}>
       <div
-        onClick={() => setCurrentPage(link)}
         className={`  cursor-pointer  transition ease-in-out  py-2 px-4 rounded-lg hover:bg-gray-800 ${
-          link == currentPage ? 'text-white' : 'text-[#98a1c0]'
+          link == pathname ? 'text-white' : 'text-[#98a1c0]'
         } `}
       >
         {title}
@@ -34,23 +33,25 @@ const NavbarItem = ({ title, link, currentPage, setCurrentPage }:ItemNavbar) => 
 
 
 function Navbar() {
-   const [currentPage, setCurrentPage] = useState<string>(location.pathname.slice(1))
+  const { pathname } = useRouter()
   return (
     <div className="sticky inset-0 z-10">
       <div className="flex flex-row items-center justify-between px-5 py-3 transition-all duration-200">
         <div className="font-semibold text-base  flex flex-row justify-center items-center">
-          <div className="flex gap-1 mr-6  text-xl justify-center items-center mx-2">
+          <Link
+            href={`/`}
+            className="flex gap-1 mr-6  text-xl justify-center items-center mx-2"
+          >
             <img src="/logo.png" alt="logo" className="h-9 w-9" />
             {/* <h1>CrossDex</h1> */}
-          </div>
+          </Link>
           <div className="flex flex-row  items-center gap-2  ">
             {objTitlelink.map((item, index) => (
               <NavbarItem
                 key={item.title + index}
                 title={item.title}
                 link={item.link}
-                currentPage={currentPage}
-                setCurrentPage={setCurrentPage}
+                pathname={pathname}
               />
             ))}
           </div>
