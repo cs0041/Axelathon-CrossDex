735bf9c356f4bdf958cdb38140d00b69dffedb88
cs0041 <pamzalove20@hotmail.com>
2023-08-31 18:16:52 +0700

    add logo website in _document

---
+++ web/pages/_document.tsx
@@ -0,0 +1,14 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <link rel="CrossDex icon" href="/logo.png" />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
